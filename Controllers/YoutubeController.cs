624d7aa4d38c966bb4607f89ee31e8a57c702b28
namigabilov <namigna@code.edu.az>
2024-09-04 00:10:38 +0400

    first commit

---
+++ Controllers/YoutubeController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VideoLibrary;
+
+namespace YoutubeDownloader.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class YoutubeController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Download(string link)
+        {
+            try
+            {
+                var youtube = YouTube.Default;
+                var video = await Task.Run(() => youtube.GetVideo(link));
+
+                if (video == null)
+                {
+                    return NotFound("Video not found.");
+                }
+
+                return File(video.GetBytes(), "video/mp4", $"{video.FullName}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
